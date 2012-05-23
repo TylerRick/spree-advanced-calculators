cb84babe6d52cbfb8d7f7394763e270baf085d8d
Tyler Rick <tyler@k3integrations.com>
2012-05-22 19:36:49 -0700

    Added a max_weight preference that is used to determine if this Calculator/ShippingMethod can be used or not.

--- app/models/calculator/weight_bucket.rb
+++ app/models/calculator/weight_bucket.rb
@@ -1,5 +1,6 @@
 class Calculator::WeightBucket < Calculator::Advanced
   preference :default_weight, :decimal, :default => 0
+  preference :max_weight,     :decimal, :default => 0
 
   def self.description
     I18n.t("weight_bucket", :scope => :calculator_names)
@@ -15,10 +16,18 @@ class Calculator::WeightBucket < Calculator::Advanced
     }.sum
   end
 
+  def available?(object)
+    return true if preferred_max_weight.nil?
+    weight = shipment_weight(object)
+    Rails.logger.debug "... #{weight} <= #{preferred_max_weight} ? => #{weight <= preferred_max_weight}"
+    weight <= preferred_max_weight
+  end
+
   # object will be Shipment
   # calculable will be ShippingMethod
   def compute(object)
-    total_weight = shipment_weight(object)
-    get_rate(total_weight) || self.preferred_default_amount
+    weight = shipment_weight(object)
+    get_rate(weight) || self.preferred_default_amount
   end
+
 end
