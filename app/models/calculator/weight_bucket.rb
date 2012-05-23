571db7393867a93a156c33bbf42037d716e7c19f
Tyler Rick <tyler@k3integrations.com>
2012-05-22 19:12:24 -0700

    Fixed NoMethodError (undefined method `map' for #<Shipment:0x0000000f0c0260>) in Calculator::WeightBucket.
    The object passed in to compute() was a Shipment, not an order. Changed
    it to always get line_items from the object passed in (Shipment).

--- app/models/calculator/weight_bucket.rb
+++ app/models/calculator/weight_bucket.rb
@@ -9,13 +9,12 @@ class Calculator::WeightBucket < Calculator::Advanced
     I18n.t('weight_unit')
   end
 
-  # as order_or_line_items we always get line items, as calculable we have Coupon, ShippingMethod or ShippingRate
-  def compute(order_or_line_items)
-    line_items = order_or_line_items.is_a?(Order) ? order_or_line_items.line_items : order_or_line_items
-    
-    total_weight = line_items.map{|li|
-        (li.variant.weight || self.preferred_default_weight) * li.quantity
-      }.sum
+  # object will be Shipment
+  # calculable will be ShippingMethod
+  def compute(object)
+    total_weight = object.line_items.map{|li|
+      (li.variant.weight || self.preferred_default_weight) * li.quantity
+    }.sum
 
     get_rate(total_weight) || self.preferred_default_amount
   end
